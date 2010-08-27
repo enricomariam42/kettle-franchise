2d86c3a06b1ec5aebb132ac7fa295a49d2d645ba
jan.aertsen@gmail.com <jan.aertsen@gmail.com@a87d24ed-a6cb-87ef-261b-3bf2e14de9dd>
2010-08-27 12:31:10 +0000

    After testing with hazadroid

--- trunk/kff/projects/templates/datawarehouse/kff_spoon_launcher.sh
+++ trunk/kff/projects/templates/datawarehouse/kff_spoon_launcher.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 clear
 
-# General parameters ############################################################################################################
-export GDK_NATIVE_WINDOWS=1
+# Date time Global Variables ####################################################
+DATE=$(date +%Y%m%d)
+TIME=$(date +%H%M%S)
+cp $HOME/.kettle/kettle.properties $HOME/.kettle/kettle.properties.$DATE.$TIME 
 
-# Parameters for sample data warehouse project ##################################################################################
+# General parameters ############################################################
+export GDK_NATIVE_WINDOWS=1
+export KETTLE_PLUGIN_PACKAGES=be.kjube
+
+# Parameters for sample data warehouse project ##################################
 CUSTOMER_NAME=templates
 PROJECT_NAME=datawarehouse
 PROJECT_DIR=/kff/projects/$CUSTOMER_NAME/$PROJECT_NAME
@@ -17,7 +23,7 @@ PROJECT_JAVA_VERSION_UAI=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java
 PROJECT_JAVA_VERSION_TST=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java
 PROJECT_JAVA_VERSION_DEV=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java
 
-# Funky menu #####################################################################################################################
+# Funky mainframe style menu ####################################################
 while true; do
     echo "****************************************************************";
     echo "* Launching Pentaho Data Integration                           *";
@@ -32,35 +38,35 @@ while true; do
     echo " "
     read -p "Enter your choice: " putd
     case $putd in
-        P )  echo "Launching PDI $PROJECT_KETTLE_VERSION_PRD for $CUSTOMER_NAME - $PROJECT_NAME (PRD)" 
+        [Pp] )  echo "Launching PDI $PROJECT_KETTLE_VERSION_PRD for $CUSTOMER_NAME - $PROJECT_NAME (PRD)" 
                 cp -T -f $PROJECT_DIR/config/configuration_PRD.properties $HOME/.kettle/kettle.properties;
                 cp -T -f /$PROJECT_DIR/config/shared.xml $HOME/.kettle/shared.xml;
-                cp -T -f /kff/software/pdi/3.2.3/ui/images/bg_banner_PRD.png /kff/software/pdi/3.2.3/ui/images/bg_banner.png; 
-                sh /kff/software/pdi/$PROJECT_KETTLE_VERSION_PRD/spoon.sh & 
+                cp -T -f /kff/images/bg_banner_PRD.png $PROJECT_KETTLE_VERSION_PRD/ui/images/bg_banner.png; 
+                sh $PROJECT_KETTLE_VERSION_PRD/spoon.sh & 
                 exit 0 
 		;;
-        U ) echo "Launching PDI 3.2.3 for customer 1 - project 1 (UAI)";
-                cp -T -f /kff/projects/customer_name/project_name/config/configuration_UAI.properties /home/jaertsen/.kettle/kettle.properties;
-                cp -T -f /kff/projects/customer_name/project_name/config/shared.xml /home/jaertsen/.kettle/shared.xml;
-                cp -T -f /kff/software/pdi/3.2.3/ui/images/bg_banner_UAI.png /kjube/software/pdi/3.2.3/ui/images/bg_banner.png; 
-                sh /kff/software/pdi/3.2.3/spoon.sh & 
+        [Uu] ) echo "Launching PDI $PROJECT_KETTLE_VERSION_PRD for $CUSTOMER_NAME - $PROJECT_NAME (UAI)";
+                cp -T -f $PROJECT_DIR/config/configuration_UAI.properties $HOME/.kettle/kettle.properties;
+                cp -T -f /$PROJECT_DIR/config/shared.xml $HOME/.kettle/shared.xml;
+                cp -T -f /kff/images/bg_banner_UAI.png $PROJECT_KETTLE_VERSION_UAI/ui/images/bg_banner.png; 
+                sh $PROJECT_KETTLE_VERSION_UAI/spoon.sh & 
                 break
 		;;
-        T ) echo "Launching PDI 3.2.3 for $CUSTOMER_NAME - $PROJECT_NAME (TST)";
-		cp -T -f $PROJECT_DIR/config/configuration_TST.properties $HOME/.kettle/kettle.properties;
-                cp -T -f $PROJECT_DIR/config/shared.xml $HOME/.kettle/shared.xml;
-                cp -T -f /kff/software/pdi/3.2.3/ui/images/bg_banner_TST.png /kff/software/pdi/3.2.3/ui/images/bg_banner.png; 
+        [Tt] ) echo "Launching PDI $PROJECT_KETTLE_VERSION_TST for $CUSTOMER_NAME - $PROJECT_NAME (TST)";
+                cp -T -f $PROJECT_DIR/config/configuration_TST.properties $HOME/.kettle/kettle.properties;
+                cp -T -f /$PROJECT_DIR/config/shared.xml $HOME/.kettle/shared.xml;
+                cp -T -f /kff/images/bg_banner_TST.png $PROJECT_KETTLE_VERSION_TST/ui/images/bg_banner.png; 
                 sh $PROJECT_KETTLE_VERSION_TST/spoon.sh & 
                 break
 		;;
-        D ) echo "Launching PDI 3.2.3 for $CUSTOMER_NAME - $PROJECT_NAME (DEV)";
+        [Dd] ) echo "Launching PDI $PROJECT_KETTLE_VERSION_DEV for $CUSTOMER_NAME - $PROJECT_NAME (DEV)";
 		cp -T -f $PROJECT_DIR/config/configuration_DEV.properties $HOME/.kettle/kettle.properties;
                 cp -T -f $PROJECT_DIR/config/shared.xml $HOME/.kettle/shared.xml;
-                cp -T -f /kff/software/pdi/3.2.3/ui/images/bg_banner_DEV.png /kff/software/pdi/3.2.3/ui/images/bg_banner.png; 
+                cp -T -f /kff/images/bg_banner_DEV.png $PROJECT_KETTLE_VERSION_TST/ui/images/bg_banner.png; 
                 sh $PROJECT_KETTLE_VERSION_DEV/spoon.sh & 
                 break
 		;;
-        * ) echo "Please input a valid environment letter combination !"
+        * ) echo "Please input a valid environment letter combination!"
             ;;
     esac
 done
