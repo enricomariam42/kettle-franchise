5b3c856655518b5904929d6526c032bdb3cd1a04
jan.aertsen <jan.aertsen@a87d24ed-a6cb-87ef-261b-3bf2e14de9dd>
2010-09-30 23:08:03 +0000

    Corrected error in generation of logging report (.xls) when there are no rejects. File creation now done in kff_logging_reports_batch_result.ktr Modifications, corrections to creation of logging tables in Oracle. Added possibility to have different shared.xml between environments. Added .spoonrc to /config directory of datawarehouse template with pointer to top level job in project. Added images for spoon canvas. Modifications to kff_spoon_launcher/kff_kitchen_launcher to cover the three above. Added logic in batch_launcher to check whether batch process with the same name is running already. If so, abort.

--- kff/projects/templates/datawarehouse/kff_kitchen_launcher.sh
+++ kff/projects/templates/datawarehouse/kff_kitchen_launcher.sh
@@ -1,53 +1,62 @@
 #!/bin/sh
 
-# Batch process (kettle job) to launch ##########################
+# Date time Global Variables ####################################################
+DATE=$(date +%Y%m%d)
+TIME=$(date +%H%M%S)
+
+# Back-up existing config files #################################################
+cp $HOME/.kettle/kettle.properties $HOME/.kettle/kettle.properties.$DATE.$TIME 
+cp $HOME/.kettle/shared.xml $HOME/.kettle/shared.xml.$DATE.$TIME 
+
+# Batch process (kettle job) to launch ##########################################
 KFF_CUSTOMER=templates ;
 KFF_APPLICATION=datawarehouse ;
 KFF_BATCH=batch_datawarehouse.kjb
+PROJECT_DIR=/kff/projects/$KFF_CUSTOMER/$KFF_APPLICATION
 
-# Quickly switch logging level  #################################################################
+# Quickly switch logging level  #################################################
 #KFF_LOG_LEVEL=None
-KFF_LOG_LEVEL=Error
+#KFF_LOG_LEVEL=Error
+#KFF_LOG_LEVEL=Minimal
 #KFF_LOG_LEVEL=Basic
-#KFF_LOG_LEVEL=Detailed
+KFF_LOG_LEVEL=Detailed
 #KFF_LOG_LEVEL=Debug
 #KFF_LOG_LEVEL=Rowlevel
 
-
-# Lifecycle management ##########################################
+# Lifecycle management ##########################################################
 case $(hostname) in
-"Jaybox")
+"Jaybox"|"hannelore")
 KFF_BASE_DIR=/kff ;
 KFF_LIFECYCLE=DEV ; 
-KFF_PDI_VERSION=/kff/software/pdi/3.2.3 ; 
-KFF_JVM_VERSION=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java ;;
+KFF_PDI_VERSION=/kff/software/pdi/4.0.0 ; 
+KFF_JVM_VERSION=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java ;
+cp -T -f $PROJECT_DIR/config/shared_DEV.xml  $HOME/.kettle/shared.xml ;;
 "test.server.be")
 echo 'This must be TST' ;
 KFF_BASE_DIR=/kff ;
 KFF_LIFECYCLE=TST ; 
 KFF_PDI_VERSION=/kff/software/pdi/3.2.3 ; 
-KFF_JVM_VERSION=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java ;;
+KFF_JVM_VERSION=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java ;
+cp -T -f $PROJECT_DIR/config/shared_TST.xml $HOME/.kettle/shared.xml ;;
 "uai.server.be")
 echo 'This must be UAI' ;
 KFF_BASE_DIR=/kff ;
 KFF_LIFECYCLE=UAI ; 
 KFF_PDI_VERSION=/kff/software/pdi/3.2.3 ; 
-KFF_JVM_VERSION=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java ;;
+KFF_JVM_VERSION=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java ;
+cp -T -f $PROJECT_DIR/config/shared_UAI.xml $HOME/.kettle/shared.xml ;; 
 "prd.server.be")
 echo 'This must be PRD'; 
 KFF_BASE_DIR=/kff ;
 KFF_LIFECYCLE=PRD ; 
 KFF_PDI_VERSION=/kff/software/pdi/3.2.3 ; 
-KFF_JVM_VERSION=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java ;;
+KFF_JVM_VERSION=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java ;
+cp -T -f $PROJECT_DIR/config/shared_PRD.xml $HOME/.kettle/shared.xml ;;
 *)
 echo "$(hostname) is not a recognized system. This must be a mistake";
 exit;;
 esac
 
-# Date time Global Variables ####################################
-DATE=$(date +%Y%m%d)
-TIME=$(date +%H%M%S)
-
-# Launch batch ##################################################
+# Launch batch ################################################################
 cd "$KFF_PDI_VERSION"
 sh kitchen.sh -file="$KFF_BASE_DIR"/projects/"$KFF_CUSTOMER"/"$KFF_APPLICATION"/code/batch_launcher.kjb -level=$KFF_LOG_LEVEL -param:KFF_CUSTOMER="$KFF_CUSTOMER" -param:KFF_APPLICATION="$KFF_APPLICATION" -param:KFF_LIFECYCLE="$KFF_LIFECYCLE" -param:KFF_BATCH="$KFF_BATCH" -logfile="$KFF_BASE_DIR"/projects/"$KFF_CUSTOMER"/"$KFF_APPLICATION"/log/"$KFF_BATCH".$DATE.$TIME.log 
--- kff/projects/templates/datawarehouse/kff_spoon_launcher.sh
+++ kff/projects/templates/datawarehouse/kff_spoon_launcher.sh
@@ -5,6 +5,7 @@ clear
 DATE=$(date +%Y%m%d)
 TIME=$(date +%H%M%S)
 cp $HOME/.kettle/kettle.properties $HOME/.kettle/kettle.properties.$DATE.$TIME 
+cp $HOME/.kettle/shared.xml $HOME/.kettle/shared.xml.$DATE.$TIME 
 
 # General parameters ############################################################
 export GDK_NATIVE_WINDOWS=1
@@ -15,9 +16,9 @@ CUSTOMER_NAME=templates
 PROJECT_NAME=datawarehouse
 PROJECT_DIR=/kff/projects/$CUSTOMER_NAME/$PROJECT_NAME
 PROJECT_KETTLE_VERSION_PRD=/kff/software/pdi/3.2.3
-PROJECT_KETTLE_VERSION_UAI=/kff/software/pdi/3.2.3
-PROJECT_KETTLE_VERSION_TST=/kff/software/pdi/3.2.3
-PROJECT_KETTLE_VERSION_DEV=/kff/software/pdi/3.2.3
+PROJECT_KETTLE_VERSION_UAI=/kff/software/pdi/3.2.5
+PROJECT_KETTLE_VERSION_TST=/kff/software/pdi/4.0.0
+PROJECT_KETTLE_VERSION_DEV=/kff/software/pdi/4.0.1
 PROJECT_JAVA_VERSION_PRD=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java
 PROJECT_JAVA_VERSION_UAI=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java
 PROJECT_JAVA_VERSION_TST=/usr/lib/jvm/java-1.6.0-openjdk/jre/bin/java
@@ -40,28 +41,32 @@ while true; do
     case $putd in
         [Pp] )  echo "Launching PDI $PROJECT_KETTLE_VERSION_PRD for $CUSTOMER_NAME - $PROJECT_NAME (PRD)" 
                 cp -T -f $PROJECT_DIR/config/configuration_PRD.properties $HOME/.kettle/kettle.properties;
-                cp -T -f /$PROJECT_DIR/config/shared.xml $HOME/.kettle/shared.xml;
+                cp -T -f /$PROJECT_DIR/config/shared_PRD.xml $HOME/.kettle/shared.xml ;
+                cp -T -f /$PROJECT_DIR/config/.spoonrc $HOME/.kettle/.spoonrc ;
                 cp -T -f /kff/images/bg_banner_PRD.png $PROJECT_KETTLE_VERSION_PRD/ui/images/bg_banner.png; 
                 sh $PROJECT_KETTLE_VERSION_PRD/spoon.sh & 
                 exit 0 
 		;;
         [Uu] ) echo "Launching PDI $PROJECT_KETTLE_VERSION_PRD for $CUSTOMER_NAME - $PROJECT_NAME (UAI)";
                 cp -T -f $PROJECT_DIR/config/configuration_UAI.properties $HOME/.kettle/kettle.properties;
-                cp -T -f /$PROJECT_DIR/config/shared.xml $HOME/.kettle/shared.xml;
+                cp -T -f /$PROJECT_DIR/config/shared_UAI.xml $HOME/.kettle/shared.xml;
+                cp -T -f /$PROJECT_DIR/config/.spoonrc $HOME/.kettle/.spoonrc ;
                 cp -T -f /kff/images/bg_banner_UAI.png $PROJECT_KETTLE_VERSION_UAI/ui/images/bg_banner.png; 
                 sh $PROJECT_KETTLE_VERSION_UAI/spoon.sh & 
                 break
 		;;
         [Tt] ) echo "Launching PDI $PROJECT_KETTLE_VERSION_TST for $CUSTOMER_NAME - $PROJECT_NAME (TST)";
                 cp -T -f $PROJECT_DIR/config/configuration_TST.properties $HOME/.kettle/kettle.properties;
-                cp -T -f /$PROJECT_DIR/config/shared.xml $HOME/.kettle/shared.xml;
+                cp -T -f /$PROJECT_DIR/config/shared_TST.xml $HOME/.kettle/shared.xml;
+                cp -T -f /$PROJECT_DIR/config/.spoonrc $HOME/.kettle/.spoonrc ;
                 cp -T -f /kff/images/bg_banner_TST.png $PROJECT_KETTLE_VERSION_TST/ui/images/bg_banner.png; 
                 sh $PROJECT_KETTLE_VERSION_TST/spoon.sh & 
                 break
 		;;
         [Dd] ) echo "Launching PDI $PROJECT_KETTLE_VERSION_DEV for $CUSTOMER_NAME - $PROJECT_NAME (DEV)";
 		cp -T -f $PROJECT_DIR/config/configuration_DEV.properties $HOME/.kettle/kettle.properties;
-                cp -T -f $PROJECT_DIR/config/shared.xml $HOME/.kettle/shared.xml;
+                cp -T -f $PROJECT_DIR/config/shared_DEV.xml $HOME/.kettle/shared.xml;
+                cp -T -f /$PROJECT_DIR/config/.spoonrc $HOME/.kettle/.spoonrc ;
                 cp -T -f /kff/images/bg_banner_DEV.png $PROJECT_KETTLE_VERSION_TST/ui/images/bg_banner.png; 
                 sh $PROJECT_KETTLE_VERSION_DEV/spoon.sh & 
                 break
